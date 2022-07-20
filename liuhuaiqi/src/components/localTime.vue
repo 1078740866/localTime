da3186f5cf1b4d35c95fe500a9f8e2e37b693f69
kan <1078740866@qq.com>
2022-07-20 17:24:17 +0900

    localTime

---
+++ liuhuaiqi/src/components/localTime.vue
@@ -0,0 +1,44 @@
+<!-- import { METHODS } from "http"; -->
+<template>
+  <div>
+    <h1>現在時刻</h1>
+    <p id="now">{{ localTime }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GenZai",
+  props: {},
+  data() {
+    return {
+      localTime: " ",
+      value: 0,
+      intervalId: null,
+    };
+  },
+  methods: {
+      // 计时器为空，操作
+      showLocaleTime: function () {
+      var time = this;
+      setInterval(function () {
+       time.localTime = new Date();
+    }, 1000);
+   }
+    },
+    // 停止定时器
+    
+  computed: {
+    now: function () {
+      return Date();
+    },
+  },
+  destroyed() {
+    // 在页面销毁后，清除计时器
+    this.clear();
+  },
+  mounted() {
+    this.showLocaleTime()
+},
+}
+</script>
